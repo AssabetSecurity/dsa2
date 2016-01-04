6e835c77ccb2c08cb78bdf89a30cea023f850c3c
george <yuramail2@rambler.ru>
2016-01-04 11:27:00 -0500

    The third test is completed.

--- server/core/src/test/java/FetchEbayTest.java
+++ server/core/src/test/java/FetchEbayTest.java
@@ -3,7 +3,9 @@ import com.assabetsecurity.core.tools.ebay.*;
 
 
 import org.junit.Test;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.util.logging.Logger;
 
@@ -23,6 +25,8 @@ public class FetchEbayTest {
 
     }
 
+
+    // returns the request as an unordered string
     @Test
     public void getResponseAsStringForTheFirstPage() throws IOException {
 
@@ -31,11 +35,25 @@ public class FetchEbayTest {
 
         EBayResponse eBayResponse = new EBayResponse(ultimateRequest);
         eBayResponse.getAsString();
-
-
-
-
     }
+
+
+    //  returns the request as an XML  structure
+    // 3 tags -  itemId, name, link to it's description
+    @Test
+    public void getResponseAsXMLForTheFirstPage() throws IOException, ParserConfigurationException, SAXException {
+
+        EBayRequest secondTest = new EBayRequest("550");
+        String ultimateRequest = secondTest.getUltimateQueryStringForPage(1);
+
+        EBayResponse eBayResponse = new EBayResponse(ultimateRequest);
+        log.info("As XML");
+        eBayResponse.getAsXML();
+    }
+
+
+
+
 
 
 
