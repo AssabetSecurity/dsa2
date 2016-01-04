60eb6b3d2072c5a6583a27d219077105729d66c0
george <yuramail2@rambler.ru>
2016-01-04 15:25:45 -0500

    Re

--- server/core/src/main/java/com/assabetsecurity/core/tools/ebay/EBayDriver.java
+++ server/core/src/main/java/com/assabetsecurity/core/tools/ebay/EBayDriver.java
@@ -1,8 +1,48 @@
 package com.assabetsecurity.core.tools.ebay;
 
+import com.assabetsecurity.core.tools.ebay.EBayRequest;
 /**
  * Created by alyas on 1/3/16.
  */
 public class EBayDriver {
-    // First commit
+
+    private String categoryId;
+    private int page;
+    private boolean lastPage;
+
+    public EBayDriver(String categoryId) {
+        this.categoryId = categoryId;
+        this.lastPage = false;
+        this.page = 0;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public boolean isLastPage() {
+        return lastPage;
+    }
+
+    public void setLastPage(boolean lastPage) {
+        this.lastPage = lastPage;
+    }
+
+    public String getItems(){
+        while (!lastPage){
+
+           //getNextPage
+
+        }
+
+        return "";
+    }
+
+
+
+
 }
