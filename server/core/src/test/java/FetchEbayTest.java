3d5e2309e1701ff581afd13d7de81040e873bac9
george <yuramail2@rambler.ru>
2016-01-04 10:04:58 -0500

    The second test is completed.

--- server/core/src/test/java/FetchEbayTest.java
+++ server/core/src/test/java/FetchEbayTest.java
@@ -11,13 +11,29 @@ public class FetchEbayTest {
 
     Logger log = Logger.getLogger(EBayRequest.class.getName());
 
+
+    // category 550, page 1
     @Test
     public void getQueryString() throws IOException {
 
-        EBayRequest connector = new EBayRequest("550");
-        String ultimateRequest = connector.getUltimateQueryStringForPage(1);
+        EBayRequest firstTest = new EBayRequest("550");
+        String ultimateRequest = firstTest.getUltimateQueryStringForPage(1);
         log.info(ultimateRequest);
         log.info("You can click the above link to get the content in browser");
+
+    }
+
+    @Test
+    public void getResponseAsStringForTheFirstPage() throws IOException {
+
+        EBayRequest secondTest = new EBayRequest("550");
+        String ultimateRequest = secondTest.getUltimateQueryStringForPage(1);
+
+        EBayResponse eBayResponse = new EBayResponse(ultimateRequest);
+        eBayResponse.getAsString();
+
+
+
 
     }
 
